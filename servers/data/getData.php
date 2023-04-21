7bbe3267a4a1cfee87cf565262dab494860b6f21
bngiahuy <45910202+bngiahuy@users.noreply.github.com>
2023-04-21 15:24:19 +0700

    Change: STATUS to CONTROL

--- servers/data/getData.php
+++ servers/data/getData.php
@@ -4,35 +4,39 @@ require_once("../Database.php");
 if (!empty($_POST['datasend'])) {
     $db = Database::getInstance();
     // $conn = $db->getConnection();
-    $datasend1 = $_POST['datasend'];
-    $value_mahoa = explode(";", $datasend1);
+    // datasend=1;1;1;0;0;0;0;4;34
+    // den1_offps1_offbom1_offrc1_offauto1?25;95;200;80;auto1
+    // String data = status_10 + ";" + status_9 + ";" + status_8 + ";" + status_7 + ";" + rain + ";" + String(t) + ";" + String(h) + ";" + String(lux) + ";" + String(mois);
 
-    $x1 = $value_mahoa[0];
-    $x2 = $value_mahoa[1];
-    $x3 = $value_mahoa[2];
-    $x4 = $value_mahoa[3];
-    $x5 = $value_mahoa[4];
-    $x6 = $value_mahoa[5];
-    $x7 = $value_mahoa[6];
-    $x8 = $value_mahoa[7];
-    $x9 = $value_mahoa[8];
+    $datasend = $_POST['datasend'];
+    $value_mahoa = explode(";", $datasend);
 
-    $sql = "UPDATE STATUS SET Trang_thai_den = $x1";
+    $x1 = $value_mahoa[0]; // status_10
+    $x2 = $value_mahoa[1]; // status_9
+    $x3 = $value_mahoa[2]; // status_8
+    $x4 = $value_mahoa[3]; // status_7
+    $x5 = $value_mahoa[4]; // rain (not used/ignore)
+    $x6 = $value_mahoa[5]; // String(t)
+    $x7 = $value_mahoa[6]; // String(h)
+    $x8 = $value_mahoa[7]; // String(lux)
+    $x9 = $value_mahoa[8]; // String(mois)
+
+    $sql = "UPDATE CONTROL SET Trang_thai_den = $x1";
     if ($result = $db->query($sql)) {
         // $result->free_result();
     }
 
-    $sql = "UPDATE STATUS SET Trang_thai_phunsuong = $x2";
+    $sql = "UPDATE CONTROL SET Trang_thai_phunsuong = $x2";
     if ($result = $db->query($sql)) {
         // $result->free_result();
     }
 
-    $sql = "UPDATE STATUS SET Trang_thai_maybom = $x3";
+    $sql = "UPDATE CONTROL SET Trang_thai_maybom = $x3";
     if ($result = $db->query($sql)) {
         // $result->free_result();
     }
 
-    $sql = "UPDATE STATUS SET Trang_thai_RC = $x4";
+    $sql = "UPDATE CONTROL SET Trang_thai_RC = $x4";
     if ($result = $db->query($sql)) {
         // $result->free_result();
     }
