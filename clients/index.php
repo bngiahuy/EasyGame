a1e4e3f50838ef1d32eaeedb258b4082ef7dab68
Trần Minh Nghĩa <85388130+trannghia0997@users.noreply.github.com>
2023-05-09 20:24:58 +0700

    just updated UI

--- clients/index.php
+++ clients/index.php
@@ -69,6 +69,7 @@ include("servers/language/config.php");
             </ul>
         </div>
     </section>
+    
     <!-- 3 image about fruit Section -->
     <section id="three-images">
         <div class="container">
